9b4f600b19d756a43508ee38dbe8bdb9855c9795
jarred <lostone1993@hotmail.com>
2021-05-01 10:52:55 +1000

    add menu system to index

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
         <link rel="stylesheet" href="./css/amber.css">
     </head>
     <body>
-        <div>
-
+        <div style="width: 99%; border: 3px dotted rgb(255, 176, 0);">
+            <div style="float: left width: 100%">
+                <h1>Local Website Menu</h1>
+                <ul style="list-style-type:none;">
+                    <li> <a href=".\websiteterminal\site">>>>Test Website Layout </a> </li>
+                </ul>
+            </div>
         </div>
     </body>
 </html>
