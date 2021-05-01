0a343433056b6898d06c2f1f74dc94fb0da7ece7
jarred <lostone1993@hotmail.com>
2021-05-01 10:44:06 +1000

    Add css from terminal website create index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/mainfont.css">
+        <link rel="stylesheet" href="./css/amber.css">
+    </head>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</html>
