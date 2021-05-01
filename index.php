cb499102bfc60553f0f144be658d7676f9143645
jarred <lostone1993@hotmail.com>
2021-05-01 12:11:04 +1000

    add menu items simplify css

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
         <link rel="stylesheet" href="./css/amber.css">
     </head>
     <body>
-        <div style="width: 99%; border: 3px dotted rgb(255, 176, 0);">
+        <div style="width: 99%;" class="main">
             <div style="float: left width: 100%">
                 <h1>Local Website Menu</h1>
                 <ul style="list-style-type:none;">
-                    <li> <a href=".\websiteterminal\site">>>>Test Website Layout </a> </li>
+                    <li> 
+                        <a href=".\websiteterminal\site">>>>Test Website Layout </a> 
+                    </li>
+                </ul>
+            </div>
+            <div style="float: left width: 100%">
+                <h1>Useful Links</h1>
+                <ul style="list-style-type:none;">
+                    <li> 
+                        <a href="http://www.google.com/">>>>Google </a> 
+                    </li>
                 </ul>
             </div>
         </div>
